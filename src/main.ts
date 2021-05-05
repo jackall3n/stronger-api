e8c9992acd59d87199dc436b45f22d12024d308e
Jack Allen <jackallensemail+git@gmail.com>
2021-05-05 18:47:55 +0100

    logging

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const logger = app.get(Logger);
   const { port = 3000 } = process.env;
 
   await app.listen(port);
+
+  logger.log(`> :${port}`);
 }
 
 bootstrap();
